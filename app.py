dcca5cff03342d134907e46b74c0c59e9fcd0090
Huido Park <dhdpark@naver.com>
2022-06-04 01:45:45 +0900

    added login.html and some css

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, session
 from cs50 import SQL
-from flask_session import Session
+#from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
@@ -10,12 +10,12 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///sports.db")
 
 @app.after_request
 def after_request(response):
@@ -25,3 +25,6 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("/login.html")
